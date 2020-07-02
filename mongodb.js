faac0eff48b7e491296e706e69e95cb6ec1ddf93
Miguel Rivas Perez <mrivasperez1996@gmail.com>
2020-07-01 21:55:55 -0700

    updates to db

--- mongodb.js
+++ mongodb.js
@@ -1,7 +1,9 @@
 // GOAL: perform crud operations in MongoDB (create, update, deleted)
 
+const 
+
 const mongodb = require("mongodb"),
-  MongoClient = mongodb.MongoClient,
+{ MongoClient, ObjectID } = require('mongodb'),
   connectionURL = "mongodb://127.0.0.1:27017",
   databaseName = "functions";
 
@@ -51,30 +53,30 @@ MongoClient.connect(
     */
     // Challenge: Use insert many to insert the documents description, completed (boolean) 2. Setup callback to handle error or print ops 3. Run the script 4. Refresh database in Robo3T and view data in tasks collection
 
-    db.collection("tasks").insertMany(
-      [
-        {
-          name: "Run",
-          description: "go for a run",
-          completed: false,
-        },
-        {
-          name: "code",
-          description: "code for an hour",
-          completed: true,
-        },
-        {
-          name: "work",
-          description: "go to work",
-          completed: false,
-        },
-      ],
-      (error, result) => {
-        if (error) {
-          console.log("an error occured");
-        }
-        console.log(result.ops);
-      }
-    );
+    // db.collection("tasks").insertMany(
+    //   [
+    //     {
+    //       name: "Run",
+    //       description: "go for a run",
+    //       completed: false,
+    //     },
+    //     {
+    //       name: "code",
+    //       description: "code for an hour",
+    //       completed: true,
+    //     },
+    //     {
+    //       name: "work",
+    //       description: "go to work",
+    //       completed: false,
+    //     },
+    //   ],
+    //   (error, result) => {
+    //     if (error) {
+    //       console.log("an error occured");
+    //     }
+    //     console.log(result.ops);
+    //   }
+    // );
   }
 );
