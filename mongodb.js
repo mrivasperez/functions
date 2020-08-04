15392bbc37c96c95a348819d789e5e6c98f05cb5
Miguel Rivas Perez <mrivasperez1996@gmail.com>
2020-08-03 21:28:49 -0700

    learned the udateone and update many methods for mongodv

--- mongodb.js
+++ mongodb.js
@@ -12,7 +12,51 @@ MongoClient.connect(
     if (error) {
       return console.log("Unable to connect to database :(");
     }
-
     const db = client.db(databaseName);
+
+    // UPDATE DOCUMENTS
+    /*
+    CHALLENGE & GOALS: use updatemany to complete all tasks
+
+    1. Check mongodb docs for updateMany
+    2. Setup the call with query and updates
+    3. Use promsie methods to setup success and error
+    4. Test it out!
+    */
+    // db.collection("tasks")
+    //   .updateMany(
+    //     {
+    //       completed: false,
+    //     },
+    //     {
+    //       $set: {
+    //         completed: true,
+    //       },
+    //     }
+    //   )
+    //   .then(result => {
+    //     console.log(result);
+    //   })
+    //   .catch(error => {
+    //     console.log(error);
+    //   });
+    //
+    // Update one
+    // if no call back is defined this method returns a promise
+    // db.collection("users")
+    //   .updateOne(
+    //     {
+    //       _id: new ObjectID("5efe018e07d7372aa2addcf0"),
+    //     },
+    //     {
+    //       $inc: { age: 33 },
+    //     }
+    //   )
+    //   .then(result => {
+    //     console.log(result);
+    //   })
+    //   .catch(error => {
+    //     console.log(error);
+    //   });
   }
 );
