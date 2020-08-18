f5b36691e4567315717c6cd46e0dfc0842a3d3ef
Miguel Rivas Perez <mrivasperez1996@gmail.com>
2020-08-17 22:12:41 -0700

    modified readme

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # const Functions = 'Under Construction';
 
 An API for task mamagement applications.
+
+npm validator
+mongoose
+mongodb
+nodeJS
--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,6 @@
-// Mongoose uses mongodb
-const mongoose = require("mongoose");
+// -- NPM Modules
+const mongoose = require("mongoose"),
+  validator = require("validator");
 
 mongoose.connect("mongodb://127.0.0.1:27017/functions", {
   useNewUrlParser: true,
@@ -12,12 +13,44 @@ mongoose.connect("mongodb://127.0.0.1:27017/functions", {
 const User = mongoose.model("User", {
   name: {
     type: String,
+    required: true,
+    trim: true,
   },
   age: {
     type: Number,
+    default: 1,
+
+    validate(value) {
+      // no negative number or 0 as age
+      if (value < 1) {
+        throw new Error("Age must be 1 or greater");
+      }
+    },
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        console.log("Your email did not pass validation.");
+      }
+    },
   },
 });
 
+const newUser = new User({
+  name: "Catherine        Milk",
+
+  email: "mrivasperez@gmail.com",
+});
+
+newUser
+  .save()
+  .then(newUser => console.log(newUser))
+  .catch(error => console.log(error));
 /*  Create a new user
 
 const me = new User({
@@ -45,25 +78,28 @@ CHALLENGE: Create a model for tasks
 4. Test your work!
 */
 
+// Create task model
+
 const Task = mongoose.model("Task", {
   description: {
     type: String,
+    required: true,
   },
   completed: {
     type: Boolean,
   },
 });
 
-const firstTask = new Task({
-  description: "Turn of the lights",
-  completed: false,
-});
+// const firstTask = new Task({
+//   description: "Turn of the lights",
+//   completed: false,
+// });
 
-firstTask
-  .save()
-  .then(firstTask => {
-    console.log("Success!", firstTask);
-  })
-  .catch(error => {
-    console.log("!ERROR!", error);
-  });
+// firstTask
+//   .save()
+//   .then(firstTask => {
+//     console.log("Success!", firstTask);
+//   })
+//   .catch(error => {
+//     console.log("!ERROR!", error);
+//   });
